3489c75a003697dc64e7a73854d95aea6a7ec56f
Sankeerth Rajesh Nair <150119555+RockRox@users.noreply.github.com>
2026-01-20 21:27:03 +0530

    Refactor packet_stream to remove random attack type

--- security_simulation/traffic_capture.py
+++ security_simulation/traffic_capture.py
@@ -1,13 +1,7 @@
 import time
-import random
-from attack_scenarios import generate_packet, ATTACK_TYPES
+from attack_scenarios import generate_packet
 
 def packet_stream():
     while True:
-        attack_type = random.choices(
-            ATTACK_TYPES,
-            weights=[0.55, 0.12, 0.08, 0.08, 0.07, 0.06, 0.04]
-        )[0]
-
-        yield generate_packet(attack_type)
+        yield generate_packet()
         time.sleep(0.05)
