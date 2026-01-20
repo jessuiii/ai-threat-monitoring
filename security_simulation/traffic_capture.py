3c09d4f76b0d87b0ae4d96d18117213c8232284d
Sankeerth Rajesh Nair <150119555+RockRox@users.noreply.github.com>
2026-01-20 17:47:25 +0530

    Refactor packet_stream to use attack types

--- security_simulation/traffic_capture.py
+++ security_simulation/traffic_capture.py
@@ -1,9 +1,13 @@
-from attack_scenarios import generate_packet
 import time
 import random
+from attack_scenarios import generate_packet, ATTACK_TYPES
 
 def packet_stream():
     while True:
-        attack = random.random() < 0.35  # 🔥 35% attack traffic
-        yield generate_packet(attack=attack)
+        attack_type = random.choices(
+            ATTACK_TYPES,
+            weights=[0.65, 0.15, 0.10, 0.10]
+        )[0]
+
+        yield generate_packet(attack_type)
         time.sleep(0.05)
